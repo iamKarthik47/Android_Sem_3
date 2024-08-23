5acb1160bbeee1f507eb7f8132131f07ae0e2a96
iamKarthik47 <63862140+iamKarthik47@users.noreply.github.com>
2024-08-23 10:31:43 +0530

    Add files via upload

---
+++ prog1/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pro1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Update IDs to match those in your XML layout
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.equals("alan") && password.equals("123")) {
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login unsuccessful. Please try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+

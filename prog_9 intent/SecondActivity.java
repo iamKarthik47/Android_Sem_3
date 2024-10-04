e696fbc4debc2b254c41683ebd656a9440b1a4b0
iamKarthik47 <63862140+iamKarthik47@users.noreply.github.com>
2024-10-04 10:26:30 +0530

    Add files via upload

---
+++ prog_9 intent/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.prog9_intentnavigation;
+
+
+import static com.example.prog9_intentnavigation.R.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView a1,a2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_second);
+        a1 = findViewById(R.id.n1);
+        a2 = findViewById(R.id.n2);
+
+        Bundle extra = getIntent().getExtras();
+        String text = extra.getString("name");
+        String text1 = extra.getString("name1");
+        a1.setText(text);
+        a2.setText(text1);
+
+
+    }
+}
